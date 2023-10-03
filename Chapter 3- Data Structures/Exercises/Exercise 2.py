d6bd0a16b92aa116129fdd77a1d6333642c90e36
safiya-salam <145570354+safiya-salam@users.noreply.github.com>
2023-10-03 19:43:56 +0400

    Exercise 2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 2.py
@@ -0,0 +1,9 @@
+names = ['Tom', 'Mike', 'Joy']
+msg = "Hello," + names[0].title() + "!"
+print(msg)
+
+msg = "Hello," + names[1].title() + "!"
+print(msg)
+
+msg = "Hello," + names[2].title() + "!"
+print(msg)
