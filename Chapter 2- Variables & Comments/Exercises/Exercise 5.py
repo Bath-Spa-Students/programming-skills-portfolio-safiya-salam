f9f886d7af9999a64918f36524ee71dbaa2f15ea
safiya-salam <145570354+safiya-salam@users.noreply.github.com>
2023-10-03 19:33:06 +0400

    Exercise 5.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 5.py
@@ -0,0 +1,4 @@
+X = int(input("Enter the sum of money you have:"))
+Y = int(input("Enter the price of one USB sticks:"))
+Z = X//Y
+print ("Total number of USB sticks you can buy whith "+str(X)+" pounds is:",Z)
