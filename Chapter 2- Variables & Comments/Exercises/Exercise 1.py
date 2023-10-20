c1aeee5f7c4e321865f8a132c40a9871ec30c860
safiya-salam <145570354+safiya-salam@users.noreply.github.com>
2023-10-20 18:29:10 +0400

    Update Exercise 1.py

--- Chapter 2- Variables & Comments/Exercises/Exercise 1.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise 1.py
@@ -1,5 +1,7 @@
+# Assign a message to a variable, and print that message.Then change the value of the variable to a new message, and print the new
+message.
+
 X = "I love learning to use Python."
 print(X)
-
 X = "It's really satisfying!"
 print(X)
