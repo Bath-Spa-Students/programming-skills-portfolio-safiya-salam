7483bfb3b403c8f9f843ac99d19c30f09df2cc1a
safiya-salam <145570354+safiya-salam@users.noreply.github.com>
2023-10-29 18:39:23 +0400

    Update practice 3

--- Chapter 1- Getting Started with Python/Exercises/practice 3.py
+++ Chapter 1- Getting Started with Python/Exercises/practice 3.py
@@ -1,4 +1,4 @@
 #You need to make a program for a leaderboard. The program needs to output the numbers. each on a separate line followed by a dot.
 
-for i in range (1, 11):
+for i in range (1, 5):
     print(i,end=".\n")
