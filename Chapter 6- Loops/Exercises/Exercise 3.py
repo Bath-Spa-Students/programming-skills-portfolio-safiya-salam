0e8e2b01c646361c99325de4bba7e4b385c52fe7
safiya-salam <145570354+safiya-salam@users.noreply.github.com>
2023-10-31 11:02:15 +0400

    Update Exercise 3.py

--- Chapter 6- Loops/Exercises/Exercise 3.py
+++ Chapter 6- Loops/Exercises/Exercise 3.py
@@ -1,5 +1,6 @@
 #Write a loop that never ends, and run it. 
 #(To end the loop, press ctrl-C or close the window displaying the output.)
 
+# This is an infinite loop that continuously prints a message
 while True:
-    print("Hi this is an infinite loop")
+    print("Hi this is an infinite loop")  # Printing a message
