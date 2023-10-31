3988a7c068665cf0afec94e56dbbcffae35fcc1e
safiya-salam <145570354+safiya-salam@users.noreply.github.com>
2023-10-31 11:02:37 +0400

    Update Exercise 4.py

--- Chapter 6- Loops/Exercises/Exercise 4.py
+++ Chapter 6- Loops/Exercises/Exercise 4.py
@@ -5,15 +5,26 @@
 #move it to the list of finished sandwiches. After all the sandwiches have been made, 
 #print a message listing each sandwich that was made.
 
+# List of sandwich orders
 sandwich_orders = ['PBNJ', 'grilled cheese', 'chicken', 'beef']
+
+# List to store finished sandwiches
 finished_sandwiches = []
 
+# Process the sandwich orders
 while sandwich_orders:
+    # Pop the last sandwich order
     sandwich = sandwich_orders.pop()
+    # Notify the working status
     print("I'm working on your " + sandwich + " sandwich.")
+    # Add finished sandwich to the list
     finished_sandwiches.append(sandwich)
 
+# Print a new line for separation
 print("\n")
+
+# Display the finished sandwiches
 for s in finished_sandwiches:
     print("I made your " + s + " sandwich.")
+
 
