3ad431711536179db55eda3d8e2357dd4623baf5
safiya-salam <145570354+safiya-salam@users.noreply.github.com>
2023-11-04 14:00:19 +0400

    Chapter 1 Exercise 3.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise 3.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 3.py
@@ -1,4 +1,4 @@
-# Write a Python program to display the current date and time.
+# Write a Python program to display the current date and time. 
 from datetime import datetime
 now = datetime.now()
 print("now =", now)
