8605a881e35cf760ba592c446cc2fda5ac779726
safiya-salam <145570354+safiya-salam@users.noreply.github.com>
2023-11-05 15:35:13 +0400

    Chapter 6 Exercise 3.py

--- Chapter 6- Loops/Exercises/Exercise 3.py
+++ Chapter 6- Loops/Exercises/Exercise 3.py
@@ -1,4 +1,4 @@
-#Write a loop that never ends, and run it. 
+#Write a loop that never ends, and run it.   
 #(To end the loop, press ctrl-C or close the window displaying the output.)
 
 # This is an infinite loop that continuously prints a message
