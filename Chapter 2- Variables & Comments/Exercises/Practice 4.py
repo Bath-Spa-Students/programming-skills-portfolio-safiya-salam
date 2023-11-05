7aa5d36f1669b449f25305931115fc92be63e524
safiya-salam <145570354+safiya-salam@users.noreply.github.com>
2023-11-05 17:26:01 +0400

    Chapter 2 Practice 4

---
+++ Chapter 2- Variables & Comments/Exercises/Practice 4.py
@@ -0,0 +1,10 @@
+#Write a python program that stores an integer and string value to variables x and y. 
+#Print the type of variable x and y.
+
+# Storing an integer and a string value to variables x and y
+x = 10
+y = "Hi"
+
+# Printing the type of variable x and y
+print("The type of variable x is:", type(x))
+print("The type of variable y is:", type(y))
