6b863b3825700a1232fd68b43a367466cf01302c
safiya-salam <145570354+safiya-salam@users.noreply.github.com>
2023-11-05 16:28:25 +0400

    Chapter 2 Practice 1

---
+++ Chapter 2- Variables & Comments/Exercises/Practice 1.py
@@ -0,0 +1,13 @@
+#Write a python code that uses three variables x, y, and z with respective values 10,20 and 30. 
+#Add all the variables and then store the result in variable a. Print the value of a.
+
+# Assigning values to variables x, y, and z
+x = 10
+y = 20
+z = 30
+
+# Adding the variables
+a = x + y + z
+
+# Printing the value of a
+print("The value of a is:", a)
