355f05ff3df6699f4a36a9447a4ba9227553f6be
safiya-salam <145570354+safiya-salam@users.noreply.github.com>
2023-11-08 09:47:02 +0400

    Chapter 2 Practice 5

---
+++ Chapter 2- Variables & Comments/Exercises/Practice 5.py
@@ -0,0 +1,13 @@
+#Write a python program that stores fruit names in a list named fruits. 
+#Unpack the list into three variables x, y and z and then print the values of each variable.
+
+# Storing fruit names in a list named fruits
+fruits = ['apple', 'banana', 'cherry']
+
+# Unpacking the list into three variables x, y, and z
+x, y, z = fruits
+
+# Printing the values of each variable
+print("x =", x)
+print("y =", y)
+print("z =", z)
