373bb155992b13c32ba3d176ad3581b1ee4d56b8
safiya-salam <145570354+safiya-salam@users.noreply.github.com>
2023-11-12 00:37:36 +0400

    Chapter 5 Practice 3

---
+++ Chapter 5- Dictionaries/Exercises/Practice 3.py
@@ -0,0 +1,8 @@
+# Write a Python program to iterate through the values of a dictionary and print them.
+
+# Sample dictionary
+my_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+
+# Iterate through the values and print them
+for value in my_dict.values():
+    print(value)
