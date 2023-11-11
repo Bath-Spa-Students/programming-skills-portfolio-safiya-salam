8a0c939859ea58787abf655153a48f37b6ae7ae8
safiya-salam <145570354+safiya-salam@users.noreply.github.com>
2023-11-11 22:55:59 +0400

    Chapter 5 Practice 2

---
+++ Chapter 5- Dictionaries/Exercises/Practice 2.py
@@ -0,0 +1,8 @@
+#Write a Python program to iterate through the keys of a dictionary and print them.
+
+# Sample dictionary
+my_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+
+# Iterate through the keys and print them
+for key in my_dict:
+    print(key)
