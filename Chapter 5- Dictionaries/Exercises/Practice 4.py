481b17862ace227dcfac087df45e9d1bcf9998a1
safiya-salam <145570354+safiya-salam@users.noreply.github.com>
2023-11-12 00:44:03 +0400

    Chapter 5 Practice 4

---
+++ Chapter 5- Dictionaries/Exercises/Practice 4.py
@@ -0,0 +1,8 @@
+#Write a Python program to iterate through both the keys and values of a dictionary and print them
+
+# Sample dictionary
+my_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+
+# Iterate through keys and values and print them
+for key, value in my_dict.items():
+    print(f"Key: {key}, Value: {value}")
