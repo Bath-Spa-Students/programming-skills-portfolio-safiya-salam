92b4d296ab6e7cbadc7a4b7d14a08ecc90f74f40
safiya-salam <145570354+safiya-salam@users.noreply.github.com>
2023-11-11 17:34:59 +0400

    Chapter 5 Practice 1

---
+++ Chapter 5- Dictionaries/Exercises/Practice 1.py
@@ -0,0 +1,20 @@
+#Use a dictionary to store information about yourself.
+
+# Define a dictionary with information about yourself
+myself_info = {
+    "name": "Safiya",
+    "age": 18,
+    "gender": "Female",
+    "occupation": "Student",
+    "hobbies": ["reading", "coding", "traveling"],
+    "location": "Ajman, UAE",
+}
+
+# Access and print information
+print("Name:", myself_info["name"])
+print("Age:", myself_info["age"])
+print("Gender:", myself_info["gender"])
+print("Occupation:", myself_info["occupation"])
+print("Hobbies:", ", ".join(myself_info["hobbies"]))
+print("Location:", myself_info["location"])
+
