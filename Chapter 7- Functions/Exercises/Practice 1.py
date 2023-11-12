46cfa06eee026ae75dc2686018dde7f772eafef8
safiya-salam <145570354+safiya-salam@users.noreply.github.com>
2023-11-12 16:28:07 +0400

    Chapter 7 Practice 1

---
+++ Chapter 7- Functions/Exercises/Practice 1.py
@@ -0,0 +1,9 @@
+#Write a Python function that takes two numbers as arguments and returns their sum.
+
+def add_numbers(num1, num2):
+    """This function takes two numbers as arguments and returns their sum."""
+    return num1 + num2
+
+# Example usage:
+result = add_numbers(3, 4)
+print("The sum is:", result)
